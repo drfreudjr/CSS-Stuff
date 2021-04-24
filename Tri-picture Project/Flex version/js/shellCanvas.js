e9158c2f8adcb2f12aa01e7f31cec42d5c3b1939
DrFreudJr <drfreudj88@gmail.com>
2021-04-24 17:24:26 -0400

    flex version

--- Tri-picture Project/Flex version/js/shellCanvas.js
+++ Tri-picture Project/Flex version/js/shellCanvas.js
@@ -2,4 +2,13 @@ const cl = console.log
 
 window.onload = function () {           // onload wrapper
  
+const carouselSlide = document.querySelector('.carousel-slide')
+const carouselImages = document.querySelectorAll('.carousel-slide img')
+
+
+
+// Buttons
+
+const prevBtn = document.querySelector('#prevBtn')
+const nextBtn = document.querySelector('#nextBtn')
 }   // end onload wrapper
