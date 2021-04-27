455df971925932ed0ad9d1f0639fcdb9f76b6d36
DrFreudJr <drfreudj88@gmail.com>
2021-04-27 19:33:47 -0400

    css js working

--- CSS-JS Interaction/js/shellCanvas.js
+++ CSS-JS Interaction/js/shellCanvas.js
@@ -1,5 +1,11 @@
 const cl = console.log
 
 window.onload = function () {           // onload wrapper
+
+elName = document.getElementById("name")
+
+cl(elName)
+
+elName.style.color = "orange"
  
 }   // end onload wrapper
