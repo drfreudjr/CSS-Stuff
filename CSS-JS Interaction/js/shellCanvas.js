65634af02b54def932362e8c147ea6517e7ba971
DrFreudJr <drfreudj88@gmail.com>
2021-04-27 20:13:24 -0400

    rf

--- CSS-JS Interaction/js/shellCanvas.js
+++ CSS-JS Interaction/js/shellCanvas.js
@@ -9,10 +9,12 @@ elName = document.getElementsByTagName('div')
 cl(elName[0])
 cl(elName[1])
 
+let sz = "500%"
+
 elName[0].onmouseover = function () {
     elName[0].style.color = "orange"
     elName[1].style.backgroundColor="red"
-    elName[1].style.fontSize = "500%"
+    elName[1].style.fontSize = sz
 }
 
 }   // end onload wrapper
