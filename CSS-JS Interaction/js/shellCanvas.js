ce48d1b30a5f36224824e2fb8aaa49340721d559
DrFreudJr <drfreudj88@gmail.com>
2021-04-27 20:11:55 -0400

    rf:

--- CSS-JS Interaction/js/shellCanvas.js
+++ CSS-JS Interaction/js/shellCanvas.js
@@ -10,7 +10,7 @@ cl(elName[0])
 cl(elName[1])
 
 elName[0].onmouseover = function () {
-    // elName.style.color = "orange"
+    elName[0].style.color = "orange"
     elName[1].style.backgroundColor="red"
     elName[1].style.fontSize = "500%"
 }
