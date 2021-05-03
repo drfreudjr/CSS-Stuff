383a4733df760af07b23c97caee48eb5e57941b4
DrFreudJr <drfreudj88@gmail.com>
2021-05-03 16:46:29 -0400

    html rf

--- My HP grid  AreaGrid/js/shellCanvas.js
+++ My HP grid  AreaGrid/js/shellCanvas.js
@@ -5,10 +5,6 @@ window.onload = function () {           // onload wrapper
 let elContainer = document.getElementsByClassName("container")
 cl(elContainer[0])
 
-elContainer[0].style.gridTemplateAreas = "'n n m m m m m m' 'r r t t c c g g' 'f f f f f f f f"
+elContainer[0].style.gridTemplateAreas = "'n n n m m m m m' 'r r t t c c g g' 'f f f f f f f f'"
 
 }   // end onload wrapper
-
-        // "n n m m m m m m"
-        // "r r t t c c g g"
-        // "f f f f f f f f";
