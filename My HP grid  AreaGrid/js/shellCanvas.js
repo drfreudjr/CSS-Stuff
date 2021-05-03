2aedb2e0ae642cd65ab91b1e18e3736334f84abd
DrFreudJr <drfreudj88@gmail.com>
2021-05-03 13:51:41 -0400

    adding js

--- My HP grid  AreaGrid/js/shellCanvas.js
+++ My HP grid  AreaGrid/js/shellCanvas.js
@@ -1,5 +1,10 @@
 const cl = console.log
 
 window.onload = function () {           // onload wrapper
- 
+
+let elContainer = document.getElementsByClassName("container")
+cl(elContainer[0])
+
+
+
 }   // end onload wrapper
