5d443dcb4f7966e3421799870ca1bba7e1bf8304
DrFreudJr <drfreudj88@gmail.com>
2021-05-04 12:30:50 -0400

    trying to get flex working

--- My HP grid  AreaGrid/js/shellCanvas.js
+++ My HP grid  AreaGrid/js/shellCanvas.js
@@ -3,7 +3,7 @@ const cl = console.log
 window.onload = function () {           // onload wrapper
 
 let elGridContainer = document.getElementsByClassName("grid-container")
-cl(elGridContainer[0])
+// cl(elGridContainer[0])
 
 elGridContainer[0].style.gridTemplateAreas = "'n n n m m m m m' 'r r t t c c g g' 'f f f f f f f f'"
 
