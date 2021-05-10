88d4196dcd992158f96c04d289418cab7a60f370
DrFreudJr <drfreudj88@gmail.com>
2021-05-10 13:28:51 -0400

    rf

--- My HP grid  AreaGrid/js/shellCanvas.js
+++ My HP grid  AreaGrid/js/shellCanvas.js
@@ -4,10 +4,11 @@ const cl = console.log
 
 window.onload = function () {           // onload wrapper
 
+// ----Layout---- //
+
     let elGridContainer = document.getElementsByClassName("grid-container")
     elGridContainer[0].style.gridTemplateAreas =    `'n n n m m m g g' 
                                                      'r r t t c c g g' 
                                                      'f f f f f f f f'`
 }   // end onload wrapper
 
-// ------End Layout ---------- //
