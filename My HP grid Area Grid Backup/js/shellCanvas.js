1f502e5e725f18d66aab985c3514415e774e689a
DrFreudJr <drfreudj88@gmail.com>
2021-05-17 16:50:35 -0400

    backup

--- My HP grid Area Grid Backup/js/shellCanvas.js
+++ My HP grid Area Grid Backup/js/shellCanvas.js
@@ -5,53 +5,46 @@ const cl = console.log
 window.onload = function () {           // onload wrapper
 
     let elGridContainer = document.getElementsByClassName("grid-container")
-    elGridContainer[0].style.gridTemplateAreas =    `'n n n m m m g g' 
-                                                     'r r t t c c g g' 
-                                                     'f f f f f f f f'`
+    // elGridContainer[0].style.gridTemplateAreas =    `'n n n m m m g g' 
+    //                                                  'c c r r t t g g' 
+    //                                                  'f f f f f f f f'`
+
+    elGridContainer[0].style.gridTemplateAreas =    `'n n n n n p m m m m m m g g g g' 
+                                                     'c c c c r r r r t t t t g g g g' 
+                                                     'f f f f f f f f f f f f g g g g'`
 
 // ----- Graphics Pane ---- //
 
 let elCanvas = document.getElementById('canvas')
 let context = canvas.getContext("2d");  
+let box = getComputedStyle(elCanvas)
 
 drawScreen()
 
-function drawScreen() {  // wrapper that gets called on resize event
+function drawScreen() {  // wrapper that gets called on resize events
     //  //  // Enter Page Specific Code here
 
-let width = 0
-let height = 0
-let iteration = 1
+// let width = elCanvas.clientWidth    // trying to figure out scroll border
+// let height = elCanvas.clientHeight
 
-let r,b,g = 0
+let width = elCanvas.offsetWidth
+let height = elCanvas.offsetHeight
 
-function crazyLines () {
+cl(width, height)
 
-    iteration++
-    cyclesPerFrame = 100000
-
-    for (let i = 0; i < cyclesPerFrame; ++i) {
-        cyclesPerFrame=cyclesPerFrame- 200
-
-        context.strokeStyle = `#303030`
-        x = Math.round(Math.random()*width*.1)
-        y = Math.round(Math.random()*height*1)
-        dx = Math.round(Math.random()*innerWidth-width*1)
-        dy = Math.round(Math.random()*innerHeight-height*2)
-
-        context.beginPath()
-        context.moveTo(x,y)
-        context.lineTo(dx,dy)
-        context.stroke()
-        context.strokeStyle = `rgb(${r+=10},${g},${b})`
-        context.fillRect(dx,dy,dx,dy)
-
-    }
-    // if (iteration < 600)
-        requestAnimationFrame(crazyLines)
+test()
+function test() {
+    context.strokeStyle  = "yellow";
+    context.lineWidth  = 10;
+    context.lineCap  = 'square';   
+    context.beginPath();   
+    context.moveTo(0, 0);   
+    context.lineTo(width-10, 0);  
+    context.stroke();   
+    // context.closePath();
 }
 
-setTimeout(crazyLines(),0)
 
 }   // end drawScreen wrapper
+
 }   // end onload wrapper
